25f02182a33322450522396b3c8d64dafb26c7ad
Vibhor <vibhor1608@gmail.com>
2017-10-28 22:27:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ashutosh/listfragmentimagestext/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ashutosh.listfragmentimagestext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
